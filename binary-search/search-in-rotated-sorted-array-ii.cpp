e2e6af6746e2f390467de16478c1b5e61c3c7de0
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2025-06-08 14:52:45 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

--- binary-search/search-in-rotated-sorted-array-ii.cpp
+++ binary-search/search-in-rotated-sorted-array-ii.cpp
@@ -1,23 +1,27 @@
 class Solution {
-    bool binarySearch(vector<int>& nums, int start, int end, int key){
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(nums[mid] == key){
-                return true;
-            }
-            if(key>nums[mid]){
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-         }
-        return false;
-    }
 public:
     bool search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size()-1;
-        sort(nums.begin(), nums.end());
-        return binarySearch(nums, 0, end, target);
+        int s = 0;
+        int e = nums.size()-1;
+        int pivot = 0;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(nums[mid] == target){
+                return true;
+            } else if(nums[mid] > target){
+                if(nums[mid] < nums[e]){
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            } else if(nums[mid] < target){
+                if(nums[mid] > nums[e]){
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            }
+        }
+        return false;
     }
 };
